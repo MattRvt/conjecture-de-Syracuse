e1493a5365b2d552e77e82748d6287f43c8ed37c
RIVAT Matthis <52105158+MattRvt@users.noreply.github.com>
2020-03-03 08:50:15 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,9 +6,16 @@
 
 int tab[3000];
 
+void conjectureIsDemontre(int resultat){
+    if (resultat == 1){
+        printf("la conjecture est démontré. le resultat = %d", resultat);
+    } else {
+        printf("la conjecture n'est pas démontré. le résultat = %d", resultat)
+    }
+}
+
 int maint(){
-
-
-
+    
+    
   return 0;
 }
