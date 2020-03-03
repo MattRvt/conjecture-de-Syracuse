34ce5a3575701ad158485f242068cfe8dc4090ab
Tidix <damien.lvzl@gmail.com>
2020-03-03 10:03:22 +0100

    misedesetapes

--- src/main.c
+++ src/main.c
@@ -11,18 +11,18 @@
 #include <stdlib.h>
 
 long int tab[3000][1];
-int valfin = 2147483647;
+//int valfin = 2147483647;
 
-int conject(int val){
+int conject(int val, int etape){
   if(val%2==0)
     tab[val][1] = val/2;
   else
     tab[val][1] = 3*val +1;
+  tab[val][0] = etape;
   printf("%ld. %d\n",tab[val][0], val);
-
-  if(val>valfin || val==1)
+  if(val==1)
     return 1;
-  conject(tab[val][1]);
+  conject(tab[val][1], etape+1);
 }
 
 int main(int argc, char *argv[]){
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]){
   int valdebut = atoi(argv[1]);
 
   if(valdebut > 0 && argc > 1){
-    conject(valdebut);
+    conject(valdebut, 1);
   }
 
   return 0;
