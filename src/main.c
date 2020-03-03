88d1b870c8050130f2e2bcf5a7e0b54de2b35fb2
Tidix <damien.lvzl@gmail.com>
2020-03-03 09:09:07 +0100

    conject2

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-
+#include <time.h>
 
 long int tab[3000][1];
+int valfin = 50;
+
 
 int conject(int val){
   if(val%2==0)
     tab[val][1] = val/2;
   else
     tab[val][1] = 3*val +1;
-
+  printf("%d : %d\n", val, tab[val][1]);
   if(val>valfin)
     return 0;
   conject(val+1);
@@ -16,10 +18,7 @@ int conject(int val){
 
 int main(){
 
-
-
-int maint(){
-
+  conject(8);
 
   return 0;
 }
