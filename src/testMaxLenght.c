1cf8f8da30b7b8232b5985d290a593866f4f733e
RIVAT Matthis <52105158+MattRvt@users.noreply.github.com>
2020-03-12 09:49:43 +0100

    update max lenght

---
+++ src/testMaxLenght.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(int argc, char *argv[])
+{
+    //longeur de tableau
+    unsigned long long i = 2094974;
+    int *pointeur;
+    int test[i];
+    while (1)
+    {
+        
+        //pointeur = malloc(2^64);
+        printf("size = %lld \n", i);
+        //free(pointeur);
+        sleep(1);
+    }
+    return 0;
+}
