641d860446e2b9873bd3215003267ecd940e7c08
Gérémy L'ECUYER <chupa4000@gmail.com>
2020-04-03 15:21:40 +0200

    #aide moi je comprend pas mon erreur

--- src/fork.c
+++ src/fork.c
@@ -9,13 +9,16 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
 #define MAX 10000000
 
-long int tab[MAX][1];
 long int memory = 0;
 //int valfin = 2147483647;
 
-int conject(long int val, int etape)
+int conject(long int val, int etape, long int *tab[][])
 {
     if (val % 2 == 0)
         tab[val][1] = val / 2;
@@ -47,12 +50,33 @@ int conject(long int val, int etape)
 
 int main(int argc, char *argv[])
 {
+    //Création IPC//
+    long int (*tab)[3];
+    key_t key;
+    key = ftok("/tmp", 'A');
+    if(key == -1){perror("ftok"); exit(EXIT_FAILURE);};
+    int id = shmget(key, 3*MAX, IPC_CREAT | IPC_EXCL | 0666);
 
+    if(id == 1){
+        switch(errno){
+            case EEXIST:
+                fprintf(stderr,"Existe déjà !\n");
+                break;
+            default:
+                perror("shmget");
+                break;
+        }
+        exit(EXIT_FAILURE);
+    }
+    tab=(long int (*)[3])shmat (id, NULL, SHM_R | SHM_W);
+    if(tab==NULL){perror("shmat"); exit(EXIT_FAILURE);}
+
+    //Traitement//
     long int valdebut = atoi(argv[1]);
 
     if (valdebut > 0 && argc > 1)
     {
-        conject(valdebut, 1);
+        conject(valdebut, 1, tab);
     }
     printf("\nCases mémoire utilisées %ld\n", memory);
 
